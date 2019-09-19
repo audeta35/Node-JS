90d088dd014c4efa93d71d51b425ba7de888f711
audeta35 <audeta35@gmail.com>
2019-09-19 16:32:37 +0700

    parse url node js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 var http = require('http');
+var url = require('url');
 var moment = require('moment');
-var salam = require('./salam');
-var server = http.createServer(function(req, res){
-
-  res.writeHead(200, {'Content-Type':'text/html'});
-  res.write('helo node');
-  res.end();
+var server = http.createServer(function(request, response){
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  var q = url.parse(request.url, true).query;
+  var txt = 'Kata kunci: ' + q.keyword;
+  response.end(txt);
 });
 
 var port = 8000;
 server.listen(port);
-
-console.log(salam.salamPagi());
 
 console.log('server berjalan di http://localhost:'+port+' pada waktu '+moment().format('D MMMM YYYY, h:mm:ss a'));
