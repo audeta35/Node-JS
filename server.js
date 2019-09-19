2cc1de8d98d471f44ef07d99556b7d9233424dd1
audeta35 <audeta35@gmail.com>
2019-09-19 08:34:29 +0700

    minor change

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var http = require('http');
+var moment = require('moment');
+var salam = require('./salam');
 var server = http.createServer(function(req, res){
 
   res.writeHead(200, {'Content-Type':'text/html'});
-  res.write('helo <b>Node</b>');
+  res.write('helo node');
   res.end();
 });
 
 var port = 8000;
 server.listen(port);
 
-console.log('server berjalan di http://localhost:'+port);
+console.log(salam.salamPagi());
+
+console.log('server berjalan di http://localhost:'+port+' pada waktu '+moment().format('D MMMM YYYY, h:mm:ss a'));
